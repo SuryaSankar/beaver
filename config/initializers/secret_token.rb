28710187047dbf70c3c99a00371cbe8624263bf8
SuryaSankar <suryashankar.m@gmail.com>
2013-07-16 19:28:52 +0530

    adding all files

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Beaver::Application.config.secret_key_base = '21f59bff2e622d184a6e27a6c78fd3f93bd1c0e33022c78260705f9cd8462f451b90b2001054de051ffc09332a3cccec816f41fbff462600da387f826c090480'
