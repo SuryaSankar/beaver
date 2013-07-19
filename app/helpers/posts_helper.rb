95c4acb157dbe8aaced6f884056dc9e9f93e5d73
SuryaSankar <suryashankar.m@gmail.com>
2013-07-20 03:55:52 +0530

    added syntax hightlighting

--- app/helpers/posts_helper.rb
+++ app/helpers/posts_helper.rb
@@ -1,2 +1,22 @@
 module PostsHelper
+=begin
+	require 'rubygems'
+	require 'redcarpet'
+	class PygmentizeHTML < Redcarpet::Render::HTML
+		def block_code(code, language)
+			require 'pygmentize'
+			require 'pygments.rb'
+			#Pygmentize.process(code, language)
+			Pygments.highlight(code, :lexer => language)
+		end
+	end
+=end
+
+	def markdown(text)
+		renderer=Redcarpet::Render::HTML.new(prettify: true)
+		extensions={ fenced_code_blocks: true }
+		#renderer2=PygmentizeHTML
+		Redcarpet::Markdown.new(renderer, extensions).render text	
+	end
+
 end
--- app/models/post.rb
+++ app/models/post.rb
@@ -1,2 +1,26 @@
 class Post < ActiveRecord::Base
 end
+=begin
+require 'rubygems'
+require 'redcarpet'
+class Post < ActiveRecord::Base
+	before_save :render_body
+
+	private
+	def render_body
+		
+		#renderer = Redcarpet::Render::HTML.new
+		renderer = PygmentizeHTML
+		extensions = {fenced_code_blocks: true}
+		redcarpet = Redcarpet::Markdown.new(renderer, extensions)
+		self.description = redcarpet.render self.description		
+	end
+end
+
+class PygmentizeHTML < Redcarpet::Render::HTML
+	def block_code(code, language)
+		require 'pygmentize'
+		Pygmentize.process(code, language)
+	end
+end
+=end
