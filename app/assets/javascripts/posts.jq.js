b8a8cd8307e08de9fb28a81fd93e3010bcdf0d18
SuryaSankar <suryashankar.m@gmail.com>
2013-07-24 16:08:20 +0530

    lots

--- app/assets/javascripts/posts.jq.js
+++ app/assets/javascripts/posts.jq.js
@@ -1,8 +1,4 @@
-/*
-(document).ready(function(){
-	MathJax.Hub.Config({       
-    	"HTML-CSS": { scale: 200}    
-  	}); 
-	MathJax.Hub.Queue(["Typeset",MathJax.Hub]);   
+$(function(){
+prettyPrint();   
 });
-*/
+
