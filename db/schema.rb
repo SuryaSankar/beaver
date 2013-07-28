e7485ab44ccbba0de54eaf2b4531ed6a8de75590
SuryaSankar <suryashankar.m@gmail.com>
2013-07-28 21:46:04 +0530

    modified posts index

--- db/schema.rb
+++ db/schema.rb
@@ -11,13 +11,14 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20130718180746) do
+ActiveRecord::Schema.define(version: 20130728153744) do
 
   create_table "posts", force: true do |t|
     t.string   "title"
     t.text     "description"
     t.datetime "created_at"
     t.datetime "updated_at"
+    t.text     "summary"
   end
 
   create_table "users", force: true do |t|
